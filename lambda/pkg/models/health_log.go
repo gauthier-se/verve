9f582178651a1edb60f3e5d5c602a33af5743add
gauthierseyzeriat <gseyzeriat1@gmail.com>
2025-09-18 21:37:47 +0200

    feat: Refactor health log management and add API documentation

--- lambda/pkg/models/health_log.go
+++ lambda/pkg/models/health_log.go
@@ -1,8 +1,8 @@
 package models
 
 type HealthLog struct {
-	UserID    string `json:"userId"`
-	Timestamp string `json:"timestamp"`
-	Type      string `json:"type"`
-	Value     string `json:"value"`
+	UserID    string `json:"userId" dynamodbav:"userId"`
+	Timestamp string `json:"timestamp" dynamodbav:"timestamp"`
+	Type      string `json:"type" dynamodbav:"type"`
+	Value     string `json:"value" dynamodbav:"value"`
 }
